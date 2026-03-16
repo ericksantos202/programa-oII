2d2571e5c2dda487e03c5784bf1c8465f98f4ba2
ericksantos202 <erickvsantos59@gmail.com>
2026-03-16 20:57:44 -0300

    sort

--- exemplo4.js
+++ exemplo4.js
@@ -1,3 +1,9 @@
 //sort//
-const nums = [2, 100, 59]; nums.sort((a, b) => a - b);
-console.log(nums);
+//const nums = [2, 100, 59]; nums.sort((a, b) => a - b); console.log(nums);//
+
+const produtos = [{nome: 'Teclado', valor: 150},
+    {nome: 'Mouse', valor: 50},
+    {nome: 'Monitor', valor: 800},];
+
+produtos.sort((a, b) => { return a.preço - b.preço;});
+console.log(produtos);
