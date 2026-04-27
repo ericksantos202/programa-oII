f74390906c625f9232d68205ec0ec1124bd614c3
ericksantos202 <erickvsantos59@gmail.com>
2026-04-27 20:40:25 -0300

    fetch

---
+++ fetch.js
@@ -0,0 +1,24 @@
+function mostrarUsuarios(lista) {
+    lista.forEach(usuario => {
+        console.log(`Nome: ${usuario.name} | Email: ${usuario.email}`);
+    });
+}
+
+async function carregarUsuarios() {
+    try {
+        const resposta = await fetch("https://jsonplaceholder.typicode.com/users");
+
+        if (!resposta.ok) {
+            throw new Error("Erro na requisição");
+        }
+
+        const dados = await resposta.json();
+
+        mostrarUsuarios(dados);
+
+    } catch (erro) {
+        console.log("Falha:", erro.message);
+    }
+}
+
+carregarUsuarios();
